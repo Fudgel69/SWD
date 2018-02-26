2ab77fe3725bbe95e501f36137c77bcd9f408e78
Fudgel69 <36034062+Fudgel69@users.noreply.github.com>
2018-02-26 23:55:51 +0100

    Update Main.cs
    Kommentarer

--- StockObserverSolution/StockObserver/Main.cs
+++ StockObserverSolution/StockObserver/Main.cs
@@ -15,22 +15,22 @@ namespace StockObserver
             Console.WriteLine("Beginning of the Program");
 
 
-            Stock Stock1 = new Stock("VESTAS", 557.50);
+            Stock Stock1 = new Stock("VESTAS", 557.50); //Creating Stock
             Stock Stock2 = new Stock("HEJSIMON", 400.56);
-            List<Stock> ss = new List<Stock>();
-            ss.Add(Stock1); ss.Add(Stock2);
+            List<Stock> ss = new List<Stock>(); //Create a list of Stocks
+            ss.Add(Stock1); ss.Add(Stock2); //Adding stocks to list
 
 
-            Portfolio PortFol = new Portfolio(ss);
+            Portfolio PortFol = new Portfolio(ss);  //Creating Portfolio
 
 
-            Stock1.RegisterObserver(PortFol);
+            Stock1.RegisterObserver(PortFol); //Register observer
             Stock2.RegisterObserver(PortFol);
-            PortFol.BuyStock("VESTAS", 5);
+            PortFol.BuyStock("VESTAS", 5); //Buy stock
             PortFol.BuyStock("HEJSIMON", 5);
-            Randomizer Rand = new Randomizer(ss);
-            Stock1.SetValueNotify(123.45);
-            PortFol.SellStock("VESTAS", 3);
+            Randomizer Rand = new Randomizer(ss); //Randomize list
+            Stock1.SetValueNotify(123.45); //Set new value to stock
+            PortFol.SellStock("VESTAS", 3); // Sell stock
 
 
             Console.WriteLine("End of the Program");
