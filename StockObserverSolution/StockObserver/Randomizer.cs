fd832875f0abebcc050b1a20435d89294808600f
Fudgel69 <36034062+Fudgel69@users.noreply.github.com>
2018-02-26 23:59:29 +0100

    Update Randomizer.cs

--- StockObserverSolution/StockObserver/Randomizer.cs
+++ StockObserverSolution/StockObserver/Randomizer.cs
@@ -11,21 +11,21 @@ namespace StockObserver
     {
         public Random rand = new Random();
 
-        public Randomizer(List<Stock> _stocks)
+        public Randomizer(List<Stock> _stocks) //Creating threads
         {
             Thread newThread = new Thread(() => Randomizer.StockRandomizer(_stocks, rand));
             newThread.Start();
         }
 
 
-        public static void StockRandomizer(List<Stock> Stocks, Random rand)
+        public static void StockRandomizer(List<Stock> Stocks, Random rand) //Randomize stock
         {
 
             while (true)
             {
                 Thread.Sleep(1000);
                 Stock last = Stocks.Last();
-                foreach (Stock s in Stocks)
+                foreach (Stock s in Stocks) //Randomize each stock in list
                 {
 
                     double incDec = (100.00 + rand.Next(-5, 5)) / 100.00;
